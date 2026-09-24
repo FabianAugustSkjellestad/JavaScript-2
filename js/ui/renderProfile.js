28cd8dd9779e0b595babd086bd5da66c377f0f0c
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-09-24 14:20:37 +0200

    Added profile header and profile picture to the renderProfile.js

--- js/ui/renderProfile.js
+++ js/ui/renderProfile.js
@@ -2,4 +2,16 @@ import { followUser, unfollowUser, getProfile } from "../api/profiles.js";
 
 export function renderProfile(profile, container) {
     container.innerHTML = "";
-    
+
+    // Profile header
+    const header = document.createElement("div");
+    header.className = "profile-header";
+
+    // Profile picture
+    if (profile.image?.url) {
+        const profileImage = document.createElement("img");
+        profileImage.src = profile.image.url;
+        profileImage.alt = `${profile.username}'s profile picture`;
+        profileImage.className = "profile-image";
+        header.appendChild(profileImage);
+    }
