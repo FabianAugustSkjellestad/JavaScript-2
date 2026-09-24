d852e1dac5d2e62c02cc17c6e56b5a92c705cc8c
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-09-24 13:41:59 +0200

    Added render author name to renderPost.js

--- js/ui/renderPost.js
+++ js/ui/renderPost.js
@@ -21,3 +21,11 @@ export function renderSinglePost(post, container) {
             avatar.className = "author-avatar";
             authorSection.appendChild(avatar);
         }
+        // Render author name, if missing fallback to "Unknown Author"
+        const authorName = document.createElement("a");
+        authorName.href = `profile.html?id=${post.author.name}`;
+        authorName.textContent = post.author.name || "Unknown";
+        authorSection.appendChild(authorName);
+
+        postElement.appendChild(authorSection);
+    }
