f64c83a69e201b810a9002bac09600a9519c3693
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-09-24 13:59:51 +0200

    Added title section, body section, tags section and image section to the renderPost.js

--- js/ui/renderPost.js
+++ js/ui/renderPost.js
@@ -3,8 +3,8 @@
 export function renderSinglePost(post, container) {
     container.innerHTML = "";
 
-    const postElement = document.createElement("div");
-    postElement.classList.add("post-element");
+    const postCard = document.createElement("div");
+    postCard.classList.add("post-card");
 
     // Author section............//
 
@@ -27,5 +27,36 @@ export function renderSinglePost(post, container) {
         authorName.textContent = post.author.name || "Unknown";
         authorSection.appendChild(authorName);
 
-        postElement.appendChild(authorSection);
+        postCard.appendChild(authorSection);
     }
+
+    // Title section
+    const postTitle = document.createElement("h2");
+    postTitle.className = "post-title";
+    postTitle.textContent = post.title || "Untitled Post";
+    postElement.appendChild(elementTitle);
+
+    // Body section
+    const postBody = document.createElement("p");
+    postBody.className = "post-body";
+    postBody.textContent = post.body || "";
+    postCard.appendChild(postBody);
+
+    // Tags section
+    if (post.tags?.length) {
+        const postTags = document.createElement("p");
+        postTags.className = "post-tags";
+        postTags.textContent = `#${post.tags.join(", #")}`;
+        postCard.appendChild(postTags);
+    }
+
+    // Image section
+    if (post.media?.url) {
+        const postImage = document.createElement("img");
+        postImage.src = post.media.url;
+        postImage.alt = post.media.alt || "Post Image";
+        postImage.className = "post-image";
+        postCard.appendChild(postImage);
+    }
+
+    // Meta section
