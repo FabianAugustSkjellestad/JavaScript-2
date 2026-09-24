06a84f093edc1fb028970a66d78ffe690cb25a2b
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-09-24 12:08:35 +0200

    Added getHeaders to the profiles.js

--- js/api/profiles.js
+++ js/api/profiles.js
@@ -1,2 +1,16 @@
 const BASE_URL = 'https://v2.api.noroff.dev/social/profiles';
 const API_KEY = "14bea214-b38d-4084-8ba8-4b906837e2ac";
+
+function getHeaders() {
+    const token = localStorage.getItem('accessToken');
+
+    if (!token) {
+        throw new Error('No access token found. Please log in first.');
+    }
+
+    return {
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+        "Content-Type": "application/json",
+    };
+}
