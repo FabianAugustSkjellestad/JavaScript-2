14e083798168694eaec048c43da62664bc5b46e1
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-09-24 11:55:00 +0200

    Added functions (getAllPosts, getPostById, searchPosts, createPost and updatePost) to the posts.js

---
+++ js/api/posts.js
@@ -0,0 +1,102 @@
+const BASE_URL = 'https://v2.api.noroff.dev/social/posts';
+const API_KEY = "14bea214-b38d-4084-8ba8-4b906837e2ac";
+
+function getHeaders() {
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+        throw new Error('No access token found. Please log in first.');
+    }
+
+    return {
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": API_KEY,
+        "Content-Type": "application/json",
+    };
+}
+
+// Fetch all posts
+/**
+ * Fetches all posts from the API, including the author information for each post.
+ * @async 
+ * @returns {Promise<Array>} A promise that resolves to an array of post objects.
+ * @throws {Error} Throws an error if the fetch request fails or if the response is not ok.
+ * 
+ * @example
+ * getAllPosts()
+ *  .then(posts => console.log(posts))
+ * .catch(error => console.error(error));
+ */
+
+export async function getAllPosts() {
+    const response = await fetch(`${BASE_URL}?_author=true`, {
+        headers: getHeaders(),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch posts.`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+// Fetch a single post by ID
+export async function getPostById(postId) {
+    const response = await fetch(`${BASE_URL}/${postId}?_author=true`, {
+        headers: getHeaders(),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch post.`);
+    }
+
+    const data = await response.json();
+    return data.data;
+}
+
+// Search posts
+export async function searchPosts(query) {
+    const response = await fetch(`${BASE_URL}?_author=true&title_contains=${query}`, {
+        headers: getHeaders(),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to search posts.`);
+    }
+
+    const data = await response.json();
+    return data.data;
+}
+
+// Create a new post
+export async function createPost(postData) {
+    const response = await fetch(BASE_URL, {
+        method: 'POST',
+        headers: getHeaders(),
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to create post.`);
+    }
+
+    const data = await response.json();
+    return data.data;
+}
+
+// Update/edit an existing post
+export async function updatePost(postId, postData) {
+    const response = await fetch(`${BASE_URL}/${postId}`, {
+        method: 'PUT',
+        headers: getHeaders(),
+        body: JSON.stringify(postData),
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to update post.`);
+    }
+
+    const data = await response.json();
+    return data.data;
+}
