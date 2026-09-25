b9320669f355c7f594ce8f9876630b6579f78616
Fabian Skjellestad <andreaswkristiansen@gmail.com>
2026-09-25 11:04:32 +0200

    Added function for live validation for email and password on login.js

--- js/main/login.js
+++ js/main/login.js
@@ -9,3 +9,14 @@ const passwordInput = document.getElementById("password");
 
 const emailError = document.getElementById("emailError");
 const passwordError = document.getElementById("passwordError");
+
+// Live validation for email and password
+emailInput.addEventListener("blur", () => {
+    const result = isValidEmail(emailInput.value);
+    emailError.textContent = result.message;
+});
+
+passwordInput.addEventListener("blur", () => {
+    const result = isValidPassword(passwordInput.value);
+    passwordError.textContent = result.message;
+});
